775808d2fc9059e0c236eb9968cf5acb841a33cf
karmanchik567 <fedukevicvlad@gmail.com>
2023-10-24 22:49:55 +0300

    init project

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const AboutPage = (props: Props) => {
+    return (
+        <div>
+            ABOUT PAGE
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const MainPage = (props: Props) => {
+    return (
+        <div>
+            MAIN PAGE
+        </div>
+    )
+}
+
+export default MainPage
