c56aaca8f31906f2f8b0c600bb0864161b6927dc
VladislavFedukevich <fedukevicvlad@gmail.com>
2023-11-02 08:42:29 +0300

    create structure && navbar

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+
+import './styles/index.scss';
+
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <AppRouter />
+        </div>
+    );
+};
+
+export default App;
